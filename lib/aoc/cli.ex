6685a24e9533f9dfb8cc0b0b676bb1245f59b98f
chriscamarillo <chriscamarillo@miami.edu>
2025-12-26 03:19:27 -0500

    formatting

--- lib/aoc/cli.ex
+++ lib/aoc/cli.ex
@@ -7,28 +7,34 @@ defmodule AOC.CLI do
       )
 
     if opts[:help] do
-       IO.puts(:stderr, usage())
-       System.halt(0)
+      IO.puts(:stderr, usage())
+      System.halt(0)
     end
 
     day = opts[:day] || error("day is required")
     part = opts[:part] || 0
-    filename = case args do
-      [name | _] -> name
-      [] -> error("input file is required")
-    end
+
+    filename =
+      case args do
+        [name | _] -> name
+        [] -> error("input file is required")
+      end
 
     solver = Module.concat([:"Day#{day}"])
+
     unless Code.ensure_loaded?(solver) do
       error("Day #{day} not implemented")
     end
 
     input = File.read!(filename)
+
     case part do
       1 ->
         solver.solve_1(input) |> IO.inspect(label: "Part 1")
+
       2 ->
         solver.solve_2(input) |> IO.inspect(label: "Part 2")
+
       _ ->
         solver.solve_1(input) |> IO.inspect(label: "Part 1")
         solver.solve_2(input) |> IO.inspect(label: "Part 2")
